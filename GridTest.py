6f8045e90de7d2b5278041a4bfa1fdc365f1212a
hortonr6 <hortonr6@coventry.ac.uk>
2020-02-05 17:34:07 +0000

    Split grid code into drawGrid function

--- GridTest.py
+++ GridTest.py
@@ -8,35 +8,36 @@ CELL_WIDTH = 20
 CELL_HEIGHT = 20
 CELL_MARGIN = 5
 
+def drawGrid(displayScreen, xSize, ySize):
+    '''Populates the surface with a grid of squares
+       displayScreen - pass in a pygame surface to draw rects on
+       xSize - integer size of the grid in the x-direction
+       ySize - integer size of the grid in the y-direction'''
+    # Loop down the y-axis
+    for row in range(0, ySize):
+        # Check if first run of loop
+        if row == 0:
+            y = CELL_MARGIN
+        else:
+            y = y + CELL_MARGIN + CELL_HEIGHT
+
+        # Loop across the x-axis
+        for column in range(0, xSize):
+            # Check if first run of loop
+            if column == 0:
+                x = CELL_MARGIN
+            else:
+                x = x + CELL_MARGIN + CELL_WIDTH
+
+            # Draw cell
+            pygame.draw.rect(displayScreen, (255, 255, 255), [x, y, CELL_HEIGHT, CELL_WIDTH])
+
 if __name__ == "__main__":
     pygame.init()
     screen = pygame.display.set_mode(WIN_SIZE)
     clock = pygame.time.Clock()
     pygame.display.set_caption("Grid Test")
-    firstLoopOuter = True
-    firstLoopInner = True
-    # Loop down the y-axis
-    for row in range(0, 10):
-        # Check if first run of loop
-        if firstLoopOuter:
-            y = CELL_MARGIN
-            firstLoopOuter = False
-        else:
-            y = y + CELL_MARGIN + CELL_HEIGHT
-
-        # Loop across the x-axis
-        for column in range(0, 10):
-            # Check if first run of loop
-            if firstLoopInner:
-                x = CELL_MARGIN
-                firstLoopInner = False
-            else:
-                x = x + CELL_MARGIN + CELL_WIDTH
-
-            # Draw cell
-            pygame.draw.rect(screen, (255, 255, 255), [x, y, CELL_HEIGHT, CELL_WIDTH])
-
-        firstLoopInner = True
+    drawGrid(screen, 10, 10)
 
     for i in range(0, 100):
         pygame.display.update()
