25b0107b11860621e3cedcbf55248027fb0629d3
hortonr6 <hortonr6@coventry.ac.uk>
2020-02-05 13:31:52 +0000

    Successfully generated a 10x10 grid

--- GridTest.py
+++ GridTest.py
@@ -13,6 +13,30 @@ if __name__ == "__main__":
     screen = pygame.display.set_mode(WIN_SIZE)
     clock = pygame.time.Clock()
     pygame.display.set_caption("Grid Test")
+    firstLoopOuter = True
+    firstLoopInner = True
+    # Loop down the y-axis
+    for row in range(0, 10):
+        # Check if first run of loop
+        if firstLoopOuter:
+            y = CELL_MARGIN
+            firstLoopOuter = False
+        else:
+            y = y + CELL_MARGIN + CELL_HEIGHT
+
+        # Loop across the x-axis
+        for column in range(0, 10):
+            # Check if first run of loop
+            if firstLoopInner:
+                x = CELL_MARGIN
+                firstLoopInner = False
+            else:
+                x = x + CELL_MARGIN + CELL_WIDTH
+
+            # Draw cell
+            pygame.draw.rect(screen, (255, 255, 255), [x, y, CELL_HEIGHT, CELL_WIDTH])
+
+        firstLoopInner = True
 
     for i in range(0, 100):
         pygame.display.update()
