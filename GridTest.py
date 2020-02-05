8591ad143e4be7447e4fa55b460fcfa430e48d8f
hortonr6 <hortonr6@coventry.ac.uk>
2020-02-05 19:14:55 +0000

    Moved functions into a class

--- GridTest.py
+++ GridTest.py
@@ -3,56 +3,67 @@
 
 import pygame
 
-WIN_SIZE = (255, 255)
+WIN_SIZE = (1005, 1005)
 CELL_WIDTH = 20
 CELL_HEIGHT = 20
 CELL_MARGIN = 5
 
-def drawGrid(displayScreen, xSize, ySize):
-    '''Populates the surface with a grid of squares
-       displayScreen - pass in a pygame surface to draw rects on
-       xSize - integer size of the grid in the x-direction
-       ySize - integer size of the grid in the y-direction'''
-    # Loop down the y-axis
-    for row in range(0, ySize):
-        # Check if first run of loop
-        if row == 0:
-            y = CELL_MARGIN
-        else:
-            y = y + CELL_MARGIN + CELL_HEIGHT
+class TestGrid:
+    def __init__(self, xSize, ySize, displayScreen):
+        self.xSize = xSize
+        self.ySize = ySize
+        self.screen = displayScreen
+        self.gridList = self.makeGridList()
 
-        # Loop across the x-axis
-        for column in range(0, xSize):
+    def makeGridList(self):
+        '''Generates a 2D array/list to store data for the displayed grid
+        xSize - integer size of the grid in the x-direction
+        ySize - integer size of the grid in the y-direction'''
+        grid = []
+        for row in range(0, self.ySize):
+            grid.append([])
+            for column in range(0, self.xSize):
+                cellLoc = str(row) + ", " + str(column)
+                grid[row].append(cellLoc)
+
+        return grid
+
+    def drawGrid(self):
+        '''Populates the surface with a grid of squares
+        displayScreen - pass in a pygame surface to draw rects on
+        xSize - integer size of the grid in the x-direction
+        ySize - integer size of the grid in the y-direction'''
+        self.screen.fill((75, 75, 75))
+        # Loop down the y-axis
+        for row in range(0, self.ySize):
             # Check if first run of loop
-            if column == 0:
-                x = CELL_MARGIN
+            if row == 0:
+                y = CELL_MARGIN
             else:
-                x = x + CELL_MARGIN + CELL_WIDTH
+                y = y + CELL_MARGIN + CELL_HEIGHT
 
-            # Draw cell
-            pygame.draw.rect(displayScreen, (255, 255, 255), [x, y, CELL_HEIGHT, CELL_WIDTH])
+            # Loop across the x-axis
+            for column in range(0, self.xSize):
+                # Check if first run of loop
+                if column == 0:
+                    x = CELL_MARGIN
+                else:
+                    x = x + CELL_MARGIN + CELL_WIDTH
 
-def makeGridList(xSize, ySize):
-    '''Generates a 2D array/list to store data for the displayed grid
-       xSize - integer size of the grid in the x-direction
-       ySize - integer size of the grid in the y-direction'''
-    gridList = []
-    for row in range(0, ySize):
-        gridList.append([])
-        for column in range(0, xSize):
-            cellLoc = str(row) + ", " + str(column)
-            gridList[row].append(cellLoc)
-
-    return gridList
+                # Draw cell
+                pygame.draw.rect(self.screen, (200, 200, 200), [x, y, CELL_HEIGHT, CELL_WIDTH])
 
 if __name__ == "__main__":
     pygame.init()
     screen = pygame.display.set_mode(WIN_SIZE)
     clock = pygame.time.Clock()
     pygame.display.set_caption("Grid Test")
-    drawGrid(screen, 10, 10)
-    grid = makeGridList(10, 10)
+    gridA = TestGrid(40, 40, screen)
+    gridA.drawGrid()
 
     for i in range(0, 100):
+        pygame.event.get()
         pygame.display.update()
         clock.tick(60)
+
+    pygame.quit()
