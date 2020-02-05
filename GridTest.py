9ce8bf54d6daa243a0ae1b9a5a2b8438073a9e2c
hortonr6 <hortonr6@coventry.ac.uk>
2020-02-05 13:31:20 +0000

    Defined grid cell dimensions

--- GridTest.py
+++ GridTest.py
@@ -4,11 +4,15 @@
 import pygame
 
 WIN_SIZE = (255, 255)
+CELL_WIDTH = 20
+CELL_HEIGHT = 20
+CELL_MARGIN = 5
 
 if __name__ == "__main__":
     pygame.init()
     screen = pygame.display.set_mode(WIN_SIZE)
     clock = pygame.time.Clock()
+    pygame.display.set_caption("Grid Test")
 
     for i in range(0, 100):
         pygame.display.update()
