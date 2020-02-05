27ef50e273ad1a5df1d7b6cf0354bc1cf14ed8b7
hortonr6 <hortonr6@coventry.ac.uk>
2020-02-05 13:30:56 +0000

    Created basic window using PyGame

--- GridTest.py
+++ GridTest.py
@@ -1,3 +1,15 @@
 # Test file for using PyGame to generate a suitable grid for displaying generated dungeon maps
 # Richard Horton 2020
 
+import pygame
+
+WIN_SIZE = (255, 255)
+
+if __name__ == "__main__":
+    pygame.init()
+    screen = pygame.display.set_mode(WIN_SIZE)
+    clock = pygame.time.Clock()
+
+    for i in range(0, 100):
+        pygame.display.update()
+        clock.tick(60)
