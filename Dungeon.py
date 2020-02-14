81e5eaee029b8a9dea8deb93fea4223349757269
hortonr6 <hortonr6@coventry.ac.uk>
2020-02-14 17:22:06 +0000

    Updated region class

--- Dungeon.py
+++ Dungeon.py
@@ -5,15 +5,24 @@ class Dungeon:
     '''Class for generating a dungeon - Represents the whole Binary Search Tree'''
     def __init__(self, rooms, xSize, ySize):
         self.numRooms = rooms
-        self.mainRegion = Region(0, 0, xSize, ySize) # Root of tree
+        # Root of the tree
+        self.mainRegion = Region(0, 0, xSize, ySize, None)
 
 class Region:
     '''Class for map region - Represents a leaf of the Binary Search Tree'''
-    def __init__(self, xHigh, yHigh, xLow, yLow):
+    def __init__(self, xHigh, yHigh, xLow, yLow, parentRegion):
         self.xTop = xHigh
         self.yTop = yHigh
         self.xLow = xLow
         self.yLow = yLow
+        self.roomLength = None
+        self.roomWidth = None
+        # Left child
+        self.subRegion1 = None
+        # Right child
+        self.subRegion2 = None
+        # Parent
+        self.parentRegion = parentRegion
 
 class Room:
     '''Class to store room data'''
