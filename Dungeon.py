2e067b023f5fa0069110ddc97ee567e324b99f98
hortonr6@coventry.ac.uk <hortonr6@coventry.ac.uk>
2020-02-14 10:06:09 +0000

    Set up dungeon module

---
+++ Dungeon.py
@@ -0,0 +1,27 @@
+# Module for dungeon generation
+# Richard Horton 2020
+
+class Dungeon:
+    '''Class for generating a dungeon - Represents the whole Binary Search Tree'''
+    def __init__(self, rooms, xSize, ySize):
+        self.numRooms = rooms
+        self.mainRegion = Region(0, 0, xSize, ySize)
+
+class Region:
+    '''Class for map region - Represents a leaf of the Binary Search Tree'''
+    def __init__(self, xHigh, yHigh, xLow, yLow):
+        self.xTop = xHigh
+        self.yTop = yHigh
+        self.xLow = xLow
+        self.yLow = yLow
+
+class Room:
+    '''Class to store room data'''
+    def __init__(self, xHigh, yHigh, xLow, yLow):
+        self.xTop = xHigh
+        self.yTop = yHigh
+        self.xLow = xLow
+        self.yLow = yLow
+
+if __name__ == "__main__":
+    print("Hello Dungeon")
