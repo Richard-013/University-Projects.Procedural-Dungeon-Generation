6d27a30f8580bfee7ff4a4dd8f6c52010468e706
hortonr6@coventry.ac.uk <hortonr6@coventry.ac.uk>
2020-03-26 18:32:07 +0000

    Update Room.py

--- Room.py
+++ Room.py
@@ -8,12 +8,20 @@ class Room:
         Takes co-ordinates of two points as tuples of positive integers,
             sorted into highest and lowest'''
     def __init__(self, lowPoint, highPoint):
+        self.minDimension = 3
         self.regionLow = lowPoint
         self.regionHigh = highPoint
         self.low = 0
         self.high = 0
 
     def generateRoom(self):
+        fullRoomChance = randint(0, 100)
+        if fullRoomChance <= 5:
+            # Room is the entire region
+            return True
+        else:
+            # Generate random area as room
+            return False
         # Decide if room will be the entire region or only some of it
         # If only some, generate room co-ordinates for high and low points
         # Set the entry point of the room
