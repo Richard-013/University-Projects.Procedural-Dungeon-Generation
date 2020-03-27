53201139746af7d065f5aef4b11e5d5100630ea3
hortonr6@coventry.ac.uk <hortonr6@coventry.ac.uk>
2020-03-27 18:13:56 +0000

    Added generateRoomLow and generateRoomHigh

--- Room.py
+++ Room.py
@@ -5,7 +5,7 @@ from random import randint
 
 class Room:
     ''' Class for holding all data on a given room in a map
-        Takes co-ordinates of two points as tuples of positive integers,
+        Takes co-ordinates of two points as tuples/arrays of positive integers,
             sorted into highest and lowest'''
     def __init__(self, lowPoint, highPoint):
         self.minDimension = 3
@@ -13,6 +13,8 @@ class Room:
         self.regionHigh = highPoint
         self.low = 0
         self.high = 0
+
+        self.generateRoom()
 
     def generateRoom(self):
         ''' Generates co-ordinates of the room within the bounds of the region'''
@@ -27,8 +29,8 @@ class Room:
             self.high = self.regionHigh
         else:
             # Generate random area as room
-            self.low = self.generateRoomLow
-            self.high = self.generateRoomHigh
+            self.low = self.generateRoomLow()
+            self.high = self.generateRoomHigh()
         # Set the entry point of the room
         self.setEntrance()
         # Set the exit point of the room
@@ -42,15 +44,36 @@ class Room:
             return True
         if ySize > 1:
             return True
+
         return False
 
     def generateRoomLow(self):
         ''' Generates co-ordinates of the lowest corner of the room'''
-        return True
+        if self.regionLow[0] == self.regionHigh[0]-3:
+            xLow = self.regionLow[0]
+        else:
+            xLow = randint(self.regionLow[0], self.regionHigh[0]-3)
+        
+        if self.regionLow[1] == self.regionHigh[1]-3:
+            yLow = self.regionLow[1]
+        else:
+            yLow = randint(self.regionLow[1], self.regionHigh[1]-3)
+
+        return (xLow, yLow)
 
     def generateRoomHigh(self):
         ''' Generates co-ordinates of the highest corner of the room'''
-        return True
+        if self.low[0]+3 == self.regionHigh[0]:
+            xHigh = self.regionHigh[0]
+        else:
+            xHigh = randint(self.low[0]+3, self.regionHigh[0])
+        
+        if self.low[1]+3 == self.regionHigh[1]:
+            yHigh = self.regionHigh[1]
+        else:
+            yHigh = randint(self.low[1]+3, self.regionHigh[1])
+
+        return (xHigh, yHigh)
 
     def setEntrance(self):
         # Set the entrance location for the room
@@ -59,3 +82,5 @@ class Room:
     def setExit(self):
         # Set the exit location for the room
         return True
+
+
