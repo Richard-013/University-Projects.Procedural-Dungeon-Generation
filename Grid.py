8ae3297bfc507d95d5ca601d3c9a79a91f50658e
hortonr6@coventry.ac.uk <hortonr6@coventry.ac.uk>
2020-04-07 17:06:23 +0100

    Created Node class

--- Grid.py
+++ Grid.py
@@ -1,6 +1,46 @@
 ''' Module for generating a data-backed grid to display a map on
     Richard Horton 2020 '''
 
+import pygame
+
+WIN_SIZE = (1000, 1000)
+CELL_WIDTH = 8
+CELL_HEIGHT = 8
+CELL_MARGIN = 2
+
+class Node():
+    ''' Class for an individual cell within the map grid'''
+    def __init__(self, x, y):
+        # Set co-ordinates of this node
+        self.x = x
+        self.y = y
+        self.parent = None  # Parent on the path found by A-Star
+        # Neighbours
+        self.up = None
+        self.down = None
+        self.left = None
+        self.right = None
+        self.neighbourList = [None, None, None, None]
+        # Set-up f,g,h values for A-Star to use
+        self.fVal = None  # G + H
+        self.gVal = None  # Distance from starting node
+        self.hVal = None  # Distance from end node
+
+    def getX(self):
+        ''' Gets the x-coordinate of this cell'''
+        return self.x
+
+    def getY(self):
+        ''' Gets the y-coordinate of this cell'''
+        return self.y
+
+    def showNeighbours(self):
+        ''' Prints this cell's neighbouring cells'''
+        print("Self: (", self.x, ",", self.y, ")")
+        print("Up: (", self.up.getX(), ",", self.up.getY(), ")")
+        print("Down: (", self.down.getX(), ",", self.down.getY(), ")")
+        print("Left: (", self.left.getX(), ",", self.left.getY(), ")")
+        print("Right: (", self.right.getX(), ",", self.right.getY(), ")")
 
 class Grid:
     ''' Grid class to hold functions and data about the grid the map is
