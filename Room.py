5e4c771b758bfcc7fe821bd5282338e4c49a0804
hortonr6@coventry.ac.uk <hortonr6@coventry.ac.uk>
2020-04-07 15:43:21 +0100

    Implemented setEntrance and setExit in Room Class

--- Room.py
+++ Room.py
@@ -14,6 +14,9 @@ class Room:
         self.low = 0
         self.high = 0
 
+        self.entrance = (0, 0)
+        self.exit = (0, 0)
+
         self.generateRoom()
 
     def generateRoom(self):
@@ -32,9 +35,9 @@ class Room:
             self.low = self.generateRoomLow()
             self.high = self.generateRoomHigh()
         # Set the entry point of the room
-        self.setEntrance()
+        self.entrance = self.setEntrance()
         # Set the exit point of the room
-        self.setExit()
+        self.exit = self.setExit()
         #print(fullRoomChance)
 
     def checkRegionSize(self):
@@ -83,12 +86,28 @@ class Room:
         return (xHigh, yHigh)
 
     def setEntrance(self):
-        # Set the entrance location for the room
-        return True
+        '''Set the entrance location for the room'''
+        # Randomly determine wall that the entrance exists on
+        axis = randint(0, 10)
+
+        if(axis > 5):
+            # Entrance is on the top wall of the room (x-axis)
+            return (randint(self.low[0], self.high[0]), self.high[1])
+        else:
+            # Entance is on the left wall of the room (y-axis)
+            return (self.low[0], randint(self.low[1], self.high[1]))
 
     def setExit(self):
-        # Set the exit location for the room
-        return True
+        '''Set the exit location for the room'''
+        # Randomly determine wall that the entrance exists on
+        axis = randint(0, 10)
+
+        if(axis > 5):
+            # Entrance is on the bottom wall of the room (x-axis)
+            return (randint(self.low[0], self.high[0]), self.low[1])
+        else:
+            # Entance is on the right wall of the room (y-axis)
+            return (self.high[0], randint(self.low[1], self.high[1]))
 
 
 if __name__ == "__main__":
@@ -96,3 +115,5 @@ if __name__ == "__main__":
     room.generateRoom()
     print(room.low)
     print(room.high)
+    print(room.entrance)
+    print(room.exit)
