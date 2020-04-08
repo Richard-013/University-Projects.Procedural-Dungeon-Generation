8b8c747d86f831c17be31c147456d9238b235e89
hortonr6@coventry.ac.uk <hortonr6@coventry.ac.uk>
2020-04-08 16:19:22 +0100

    Improved blockedCells list

--- AStar.py
+++ AStar.py
@@ -111,7 +111,14 @@ class AStar():
         for x in range(0, self.grid.xSize):
             for y in range(0, self.grid.ySize):
                 if self.grid.gridCells[x][y].blocked is True:
-                    self.closedCells.append(self.grid.gridCells[x][y])
+                    # If a cell is marked as blocked, add its coordinates to the blocked cells list
+                    self.blockedCells.append((x, y))
+                elif x == 0 or y == 0:
+                    # Prevent pathfinding following the edges of the map
+                    self.blockedCells.append((x, y))
+                elif x == self.grid.xSize-1 or y == self.grid.ySize-1:
+                    # Prevent pathfinding following the edges of the map
+                    self.blockedCells.append((x, y))
 
     def findPath(self):
         ''' Finds the path using A*'''
