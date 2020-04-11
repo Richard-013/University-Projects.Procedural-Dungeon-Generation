44dabee4758556dcc1f6c3344ba14f3be81115c2
hortonr6 <hortonr6@coventry.ac.uk>
2020-04-11 16:04:58 +0100

    Added dungeon type and output file

--- Main.py
+++ Main.py
@@ -1,13 +1,22 @@
+''' Main file used to run the procedural generation sytem'''
+
+from random import randint
 import pygame
 import Grid
 
 WIN_SIZE = (1000, 1000)
+OUTPUT_FILE_NAME = "Keywords.txt"
+
+outputFile = open(OUTPUT_FILE_NAME, 'w')
+outputFile.write("Dungeon Keywords:\n")
+outputFile.close()
 
 mapSizeX = 100
 mapSizeY = 100
 maxRoomArea = 1000
 minRoomDimension = 15
 pathRender = False
+dungeonType = "Dungeon"
 
 print("Please enter the horizontal size of the map: ")
 mapSizeX = int(input())
@@ -17,8 +26,22 @@ print("Please enter the maximum area one room should occupy: ")
 maxRoomArea = int(input())
 print("Please enter the minimum dimension a room should have: ")
 minRoomDimension = int(input())
+print("What type of dungeon would you like?\n1 - Tomb\n2 - Ruin\n3 - Dungeon\n4 - Forest\n5 - Random\n")
+dungeonType = int(input())
 print("Do you want auto-generated paths between the rooms: Y/N ")
 pathRenderInput = input()
+
+if dungeonType == 5:
+    dungeonType = randint(0, 4)
+
+if dungeonType == 1:
+    dungeonType = "Tomb"
+elif dungeonType == 2:
+    dungeonType = "Ruin"
+elif dungeonType == 3:
+    dungeonType = "Dungeon"
+elif dungeonType == 4:
+    dungeonType = "Forest"
 
 if pathRenderInput == "Y" or pathRenderInput == "y":
     pathRender = True
@@ -30,7 +53,7 @@ screen = pygame.display.set_mode(WIN_SIZE)
 clock = pygame.time.Clock()
 pygame.display.set_caption("Grid Test")
 mapGrid = Grid.Grid(mapSizeX, mapSizeY, screen, pathRender)
-mapGrid.createMap(maxRoomArea, minRoomDimension)
+mapGrid.createMap(maxRoomArea, minRoomDimension, dungeonType)
 
 while True:
     for event in pygame.event.get():
